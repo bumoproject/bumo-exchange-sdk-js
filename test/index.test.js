ffedb3f7542bd353bbb32c7faa7144f20c8462ce
李磊 <lilei@bumo.cn>
2018-05-29 10:34:16 +0800

    modified test/index.test.js

--- test/index.test.js
+++ test/index.test.js
@@ -11,8 +11,7 @@ const bumo = new BumoSDK({
 
 
 describe('Test bumo-exchange-sdk', function() {
-
-  it('test: account.create', function() {
+  it('test: account.create', function(done) {
     bumo.account.create().then(data => {
       const obj = data;
       obj.should.be.a('object');
@@ -28,77 +27,77 @@ describe('Test bumo-exchange-sdk', function() {
       obj.data.publicKey.should.be.a('string');
       obj.data.address.should.be.a('string');
 
-      const checkPrivateKey = keypair.checkEncPrivateKey(obj.data.privateKey)
-      const checkPublickKey = keypair.checkEncPublicKey(obj.data.publicKey)
-      const checkAddress = keypair.checkAddress(obj.data.address)
+      const checkPrivateKey = keypair.checkEncPrivateKey(obj.data.privateKey);
+      const checkPublickKey = keypair.checkEncPublicKey(obj.data.publicKey);
+      const checkAddress = keypair.checkAddress(obj.data.address);
       checkPrivateKey.should.equal(true);
       checkPublickKey.should.equal(true);
       checkAddress.should.equal(true);
+			done();
     }).catch(err => {
-      console.log('++++++++++++');
-      console.log(err);
-      console.log('++++++++++++');
-      // should.not.Throw
+      done(err);
     });
   });
 
-  it('test: account.getBalance', function() {
+  it('test: account.getBalance', function(done) {
     co(function* () {
       try {
         const info = yield bumo.account.getBalance('buQXz2qbTb3yx2cRyCz92EnaUKHrwZognnDw');
-        const account = yield bumo.account.getBalance('buQsBMbFNH3NRJBbFRCPWDzjx7RqRc1hhvn1')
+        const account = yield bumo.account.getBalance('buQsBMbFNH3NRJBbFRCPWDzjx7RqRc1hhvn1');
         info.should.be.a('object');
         info.should.have.property('error_code');
         info.should.have.property('msg');
         info.should.have.property('data');
         info.error_code.should.not.equal(0);
         account.error_code.should.equal(0);
+				done();
       } catch (err) {
-        console.log(err.message);
+				done(err);
       }
     }).catch(err => {
-      console.log(err.message);
+      done(err);
     });
   });
 
-  it('test: account.getInfo', function() {
+  it('test: account.getInfo', function(done) {
     co(function* () {
       try {
         const info = yield bumo.account.getInfo('buQXz2qbTb3yx2cRyCz92EnaUKHrwZognnDw');
         // const account = yield bumo.account.getInfo('buQsBMbFNH3NRJBbFRCPWDzjx7RqRc1hhvn1')
-        const account = yield bumo.account.getInfo('buQaKVttFz5wSGWErbCnQteY6qTbSHkTouVr')
+        const account = yield bumo.account.getInfo('buQaKVttFz5wSGWErbCnQteY6qTbSHkTouVr');
         info.should.be.a('object');
         info.should.have.property('error_code');
         info.should.have.property('msg');
         info.should.have.property('data');
         info.error_code.should.not.equal(0);
         account.error_code.should.equal(0);
-        // console.log(account);
+        done();
       } catch (err) {
-        console.log(err.message);
+				done(err);
       }
     }).catch(err => {
-      console.log(err.message);
+      done(err);
     });
 
   });
 
-  it('test: getTransaction', function() {
+  it('test: getTransaction', function(done) {
     const hash = 'e27d287913dcbe5452d38a567b10f6b73a2a22a2f3c393180ab930286eb8ffd9';
     co(function* () {
       try {
         const result = yield bumo.getTransaction(hash);
         result.error_code.should.equal(0);
+				done();
       } catch (err) {
-        console.log(err.message);
+				done(err);
       }
     }).catch(err => {
-      console.log(err);
+      done(err);
     });
 
   });
 
-  it('test: getBlock', function() {
+  it('test: getBlock', function(done) {
     co(function* () {
       try {
         const blockNumber = 100;
@@ -108,61 +107,62 @@ describe('Test bumo-exchange-sdk', function() {
         const data = yield bumo.getBlock(blockNumberNotExist);
         result.error_code.should.equal(0);
         data.error_code.should.not.equal(0);
-
+				done();
       } catch (err) {
-        console.log(err.message);
+        done(err);
       }
     }).catch(err => {
-      console.log(err);
+      done(err);
     });
 
   });
 
-  it('test: checkBlockStatus', function() {
+  it('test: checkBlockStatus', function(done) {
     co(function* () {
       try {
         const result = yield bumo.checkBlockStatus();
-        // console.log(result);
-        // console.log(result);
         result.data.should.be.a('boolean');
+				done();
       } catch (err) {
-        console.log(err.message);
+        done(err);
       }
     }).catch(err => {
-      console.log(err);
+      done(err);
     });
 
   });
 
-  it('test: getBlockNumber', function() {
+  it('test: getBlockNumber', function(done) {
     co(function* () {
       try {
         const result = yield bumo.getBlockNumber();
         result.data.seq.should.be.a('number');
+				done();
       } catch (err) {
-        console.log(err.message);
+        done(err);
       }
     }).catch(err => {
-      console.log(err);
+      done(err);
     });
 
   });
 
-  it('test: account.checkAddress', function() {
+  it('test: account.checkAddress', function(done) {
     co(function* () {
       try {
         const result = yield bumo.account.checkAddress('buQXz2qbTb3yx2cRyCz92EnaUKHrwZognnDw');
         result.data.should.equal(true);
+				done();
       } catch (err) {
-        console.log(err.message);
+				done(err);
       }
     }).catch(err => {
-      console.log(err);
+      done(err);
     });
 
   });
 
-  it('test: sendBu', function() {
+  it('test: sendBu', function(done) {
     co(function* () {
       try {
         const options = {
@@ -178,15 +178,12 @@ describe('Test bumo-exchange-sdk', function() {
         const data = yield bumo.sendBu(options);
         // console.log(data);
         data.error_code.should.not.equal(0);
-
+				done();
       } catch (err) {
-        console.log(err);
-        // console.log(err.message);
+				done(err);
       }
     }).catch(err => {
-      console.log(err.message);
+      done(err);
     });
   });
-
-
 });
